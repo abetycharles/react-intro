603995b405961b4d00eb5df6db8bacbb7aad3015
RVHU321012UR <RVHU321012UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-11-20 20:48:55 -0500

    starter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function formatName(user){
+  return user.firstName + ' ' + user.lastName;
+}
 function App() {
+  const user = {
+    firstName: "Charles",
+    lastName: "Abety"
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +17,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Hello, my name is  {user.firstName} {user.lastName} </p>
+        <p>Also, i am fancy: {formatName(user)}</p>
+        <p>With ES6... look at you -:) {`${user.firstName} ${user.lastName}`}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
